d2820588dddeb5cdf42a45e2a4c4f3ff7339c13a
icaromx <ivan.caro.t@gmail.com>
2018-08-15 13:40:04 -0500

    Added script

--- source_me_first.sh
+++ source_me_first.sh
@@ -4,6 +4,9 @@ if [[ "$HOSTNAME" == *"uboonegpvm"* ]]; then
 	setup root v6_12_06a -q e15:prof
 fi
 
+mkdir ../build_Michel_Reco/
+cd ../build_Michel_Reco/
+cmake ../Michel_Reco/
 make
 
 export MICHELRECOPATH=`pwd`
