2bc46ba8b268876b378e59d1b8a0ad3dbac9ad0a
icaromx <ivan.caro.t@gmail.com>
2018-08-15 12:18:11 -0500

    setup script updated

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 # get access to larsoft and uboonecode products
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	echo "using Linux"
+	echo "Using Linux"
 	source /grid/fermiapp/products/uboone/setup_uboone.sh
 	setup cmake v3_10_1
 	setup root v6_12_06a -q e15:prof
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Using macOS"
 fi
+
+mkdir ../build_Michel_Reco/
+cd ../build_Michel_Reco/
 cmake ../Michel_Reco/
 make
 
