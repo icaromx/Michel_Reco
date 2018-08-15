9b6272b1e34e0ae0feaf970d8c116ac9860fe49c
icaromx <ivan.caro.t@gmail.com>
2018-08-15 12:13:55 -0500

    setup script updated

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 # get access to larsoft and uboonecode products
-source /grid/fermiapp/products/uboone/setup_uboone.sh
-setup cmake v3_10_1
-setup root v6_12_06a -q e15:prof
-
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	echo "using Linux"
+	source /grid/fermiapp/products/uboone/setup_uboone.sh
+	setup cmake v3_10_1
+	setup root v6_12_06a -q e15:prof
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Using macOS"
+fi
+cmake ../Michel_Reco/
 make
 
 export MICHELRECOPATH=`pwd`
